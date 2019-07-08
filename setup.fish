2a307a49e78c2501fddf8f2e2bc6b49865458ee6
Andy Klimczak <andyklimczak@fastmail.com>
2019-07-08 14:08:23 -0400

    add fishfile

--- setup.fish
+++ setup.fish
@@ -3,5 +3,6 @@ ln -s ~/cfg/.tmux.conf ~
 ln -s ~/cfg/.gitconfig ~
 ln -s ~/cfg/.pryrc ~
 ln -s ~/cfg/.gitignore ~
+ln -s ~/cfg/fishfile ~/.config/fish
 
 . ~/cfg/andy.fish > ~/.config/fish/config.fish
