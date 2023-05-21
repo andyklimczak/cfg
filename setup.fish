77bc860fa5ba8f17a7f7589ac970878905242cdd
Andy Klimczak <andyklimczak@fastmail.com>
2023-05-21 16:04:41 +0100

    .gemrc

--- setup.fish
+++ setup.fish
@@ -3,6 +3,7 @@ ln -s ~/cfg/.tmux.conf ~
 ln -s ~/cfg/.gitconfig ~
 ln -s ~/cfg/.pryrc ~
 ln -s ~/cfg/.gitignore ~
+ln -s ~/cfg/.gemrc ~
 ln -s ~/cfg/fishfile ~/.config/fish
 
 . ~/cfg/andy.fish > ~/.config/fish/config.fish
